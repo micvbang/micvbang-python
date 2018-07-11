63130be44279c03676d0f79051818015942d3e54
Michael Bang <michael@katnegermis.dk>
2018-07-11 16:10:30 +0200

    io: fix bug in list_dir, and add recursive option.

--- micvbang/io.py
+++ micvbang/io.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import gzip
+import stat
 
 
 def here(*ps):
@@ -9,19 +10,41 @@ def here(*ps):
     return os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), *ps)
 
 
-def list_dir(path, dirs=False, files=True, ext=None):
-    """ List the contents of a directory.
-
-    Returns directories if dirs is set.
-    Returns files if files is set.
-    Returns only files with the given extension if ext is set.
-    """
+def _list_dir(path, dirs, files, ext):
     for fname in os.listdir(path):
-        d = dirs and os.path.isdir(fname)
-        f = files and os.path.isfile(fname)
-        e = ext is None or os.path.splitext(fname)[1] == ext
-        if d or f or e:
-            yield os.path.join(path, fname)
+        fpath = os.path.join(path, fname)
+        mode = os.stat(fpath).st_mode
+
+        isdir = stat.S_ISDIR(mode)
+        isfile = stat.S_ISREG(mode)
+
+        d = dirs and isdir
+        f = files and isfile and (ext is None or os.path.splitext(fname)[1] == ext)
+
+        if d or f:
+            yield fpath
+
+
+def list_dir(path='.', dirs=False, files=True, ext=None, recursive=False):
+    """ List the contents of a directory.
+    If recursive is set to True, list_dir will recurse through sub-dirs as
+    well.
+
+    Returns directories if dirs is set to True.
+    Returns files if files is set True.
+    Returns only files with the given extension if ext is set. Note that
+        the extension includes the dot, i.e. '.jpg'.
+    """
+    if not recursive:
+        yield from _list_dir(path, dirs, files, ext)
+        return
+
+    yield from _list_dir(path, dirs=False, files=True, ext=ext)
+
+    for d in _list_dir(path, dirs=True, files=False, ext=None):
+        if dirs:
+            yield d
+        yield from list_dir(d, dirs, files, ext, recursive=True)
 
 
 def open_file(path, mode='r'):
@@ -32,5 +55,5 @@ def open_file(path, mode='r'):
     _, ext = os.path.splitext(path)
     if ext == '.gz':
         return gzip.open(path, mode)
-    else:
-        return open(path, mode)
+
+    return open(path, mode)
